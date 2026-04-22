1f97e4cc5cd5a476078fbf4b5667cba54ac38789
Kristin0 <kris123kris99@gmail.com>
2026-04-22 16:19:16 +0400

    add nginx, systemd unit

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-# -- Setup and run Flask app with Gunicorn --
+# -- Setup Flask app (install dependencies only) --
 python3 -m venv venv/
 source venv/bin/activate
 pip3 install -r requirements.txt
-gunicorn --worker-class gthread --workers 1 --threads 4 --bind 127.0.0.1:5003 app:app
 
-
-# Copy to nginx sites
-sudo cp nginx.conf /etc/nginx/sites-available/chatbot
 sudo ln -s /etc/nginx/sites-available/chatbot /etc/nginx/sites-enabled/chatbot
 
 # Test and reload
 sudo nginx -t
 sudo systemctl reload nginx
+
+# -- Setup systemd service --
+sudo cp cv.service /etc/systemd/system/cv.service
+sudo systemctl daemon-reload
+sudo systemctl enable cv
+sudo systemctl restart cv   
