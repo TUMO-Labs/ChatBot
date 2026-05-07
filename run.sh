ab1bea2f0e44e48d95e0d779aee9dd85e0163b4e
Kristin0 <kris123kris99@gmail.com>
2026-05-07 21:51:14 +0400

    update run script

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# -- Setup Flask app (install dependencies only) --
+set -e
+
+# -- Install system dependencies --
+sudo apt update
+sudo apt install -y python3-venv python3-pip nginx
+
+# -- Setup Python venv --
 python3 -m venv venv/
 source venv/bin/activate
 pip3 install -r requirements.txt
@@ -9,10 +15,11 @@ sudo rm -f /etc/nginx/sites-enabled/cv.conf
 sudo cp nginx/cv.conf /etc/nginx/sites-available/cv.conf
 sudo ln -s /etc/nginx/sites-available/cv.conf /etc/nginx/sites-enabled/cv.conf
 sudo nginx -t
-sudo systemctl reload nginx
+sudo systemctl enable nginx
+sudo systemctl restart nginx
 
 # -- Setup systemd service --
 sudo cp cv.service /etc/systemd/system/cv.service
 sudo systemctl daemon-reload
 sudo systemctl enable cv
-sudo systemctl restart cv   
+sudo systemctl restart cv
